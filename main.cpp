eadfe133188a264015a67dd8796899fd455f3f95
kerem-kuru <keremkuru2007@gmail.com>
2026-05-23 14:48:08 +0300

    Sistem: Oyun durumlari (Menu, Playing, Game Over) ve baslat butonu eklendi

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@ const int MINE_COUNT = 15;
 const float CELL_WIDTH = (float)WINDOW_WIDTH / COLS;
 const float CELL_HEIGHT = (float)WINDOW_HEIGHT / ROWS;
 
+// YENİ: Oyun Durumları Eklendi
+enum GameState { MENU, PLAYING, GAME_OVER };
+GameState currentState = MENU;
+
 struct Cell {
     bool isMine = false;
     bool isRevealed = false;
@@ -23,7 +27,6 @@ struct Cell {
 };
 
 std::vector<std::vector<Cell>> board;
-bool gameOver = false;
 TTF_Font* mainFont = nullptr;
 
 // --- FONKSİYONLAR ---
@@ -58,7 +61,6 @@ void revealCell(int r, int c) {
     }
 }
 
-// Ekrana Yazı Yazdıran Fonksiyon
 void drawText(SDL_Renderer* renderer, std::string text, float x, float y, SDL_Color color) {
     if (!mainFont) return;
     SDL_Surface* surf = TTF_RenderText_Blended(mainFont, text.c_str(), 0, color);
@@ -71,60 +73,80 @@ void drawText(SDL_Renderer* renderer, std::string text, float x, float y, SDL_Co
 
 int main(int argc, char* argv[]) {
     if (!SDL_Init(SDL_INIT_VIDEO)) return -1;
-    if (TTF_Init() == -1) return -1; // TTF Başlatıldı
+    if (TTF_Init() == -1) return -1;
 
-    // Fontu Yükle
     mainFont = TTF_OpenFont("C:\\Windows\\Fonts\\arial.ttf", 24);
-    if (!mainFont) {
-        std::cout << "Font yuklenemedi!" << std::endl;
-    }
+    if (!mainFont) std::cout << "Font yuklenemedi!" << std::endl;
 
     SDL_Window* window = SDL_CreateWindow("Mayin Tarlasi - Kerem", WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
 
-    initBoard();
+    // YENİ: Başlat Butonu
+    SDL_FRect startButton = { 150, 450, 300, 60 };
     bool isRunning = true;
     SDL_Event event;
 
     while (isRunning) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) isRunning = false;
-            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN && !gameOver) {
-                int c = (int)(event.button.x / CELL_WIDTH);
-                int r = (int)(event.button.y / CELL_HEIGHT);
-                if (event.button.button == SDL_BUTTON_LEFT) {
-                    if (board[r][c].isMine) {
-                        gameOver = true;
-                        for(int i=0; i<ROWS; i++) for(int j=0; j<COLS; j++) if(board[i][j].isMine) board[i][j].isRevealed = true;
-                    } else revealCell(r, c);
-                } else if (event.button.button == SDL_BUTTON_RIGHT) board[r][c].isFlagged = !board[r][c].isFlagged;
+
+            // YENİ: Durumlara Göre Tıklama Mantığı
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                if (currentState == MENU) {
+                    if (event.button.x >= startButton.x && event.button.x <= startButton.x + startButton.w &&
+                        event.button.y >= startButton.y && event.button.y <= startButton.y + startButton.h) {
+                        initBoard();
+                        currentState = PLAYING;
+                    }
+                } else if (currentState == PLAYING) {
+                    int c = (int)(event.button.x / CELL_WIDTH);
+                    int r = (int)(event.button.y / CELL_HEIGHT);
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        if (board[r][c].isMine) {
+                            currentState = GAME_OVER;
+                            for(int i=0; i<ROWS; i++) for(int j=0; j<COLS; j++) if(board[i][j].isMine) board[i][j].isRevealed = true;
+                        } else revealCell(r, c);
+                    } else if (event.button.button == SDL_BUTTON_RIGHT) {
+                        board[r][c].isFlagged = !board[r][c].isFlagged;
+                    }
+                } else if (currentState == GAME_OVER) {
+                    currentState = MENU; // Ekrana tıklayınca menüye dön
+                }
             }
         }
 
         SDL_SetRenderDrawColor(renderer, 20, 20, 20, 255);
         SDL_RenderClear(renderer);
 
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                SDL_FRect rect = { c * CELL_WIDTH + 1, r * CELL_HEIGHT + 1, CELL_WIDTH - 2, CELL_HEIGHT - 2 };
-                if (board[r][c].isRevealed) {
-                    if (board[r][c].isMine) {
-                        SDL_SetRenderDrawColor(renderer, 220, 50, 50, 255);
-                    } else {
-                        SDL_SetRenderDrawColor(renderer, 180, 180, 180, 255);
-                        SDL_RenderFillRect(renderer, &rect);
-
-                        // Komşu mayın sayısını ekrana yazdır (0'dan büyükse)
-                        if (board[r][c].neighborMines > 0) {
-                            drawText(renderer, std::to_string(board[r][c].neighborMines), c * CELL_WIDTH + 25, r * CELL_HEIGHT + 15, {0, 0, 0, 255});
+        // YENİ: Durumlara Göre Çizim
+        if (currentState == MENU) {
+            drawText(renderer, "MAYIN TARLASI", 210, 100, {255, 255, 255, 255});
+            SDL_SetRenderDrawColor(renderer, 0, 180, 0, 255);
+            SDL_RenderFillRect(renderer, &startButton);
+            drawText(renderer, "OYUNU BASLAT", startButton.x + 55, startButton.y + 15, {255, 255, 255, 255});
+        } else {
+            for (int r = 0; r < ROWS; r++) {
+                for (int c = 0; c < COLS; c++) {
+                    SDL_FRect rect = { c * CELL_WIDTH + 1, r * CELL_HEIGHT + 1, CELL_WIDTH - 2, CELL_HEIGHT - 2 };
+                    if (board[r][c].isRevealed) {
+                        if (board[r][c].isMine) {
+                            SDL_SetRenderDrawColor(renderer, 220, 50, 50, 255);
+                        } else {
+                            SDL_SetRenderDrawColor(renderer, 180, 180, 180, 255);
+                            SDL_RenderFillRect(renderer, &rect);
+                            if (board[r][c].neighborMines > 0)
+                                drawText(renderer, std::to_string(board[r][c].neighborMines), c * CELL_WIDTH + 25, r * CELL_HEIGHT + 15, {0, 0, 0, 255});
+                            continue;
                         }
-                        continue; // Yazıyı çizdikten sonra diğer dikdörtgeni çizmesin diye
+                    } else {
+                        SDL_SetRenderDrawColor(renderer, 70, 70, 70, 255);
+                        if (board[r][c].isFlagged) SDL_SetRenderDrawColor(renderer, 255, 140, 0, 255);
                     }
-                } else {
-                    SDL_SetRenderDrawColor(renderer, 70, 70, 70, 255);
-                    if (board[r][c].isFlagged) SDL_SetRenderDrawColor(renderer, 255, 140, 0, 255);
+                    SDL_RenderFillRect(renderer, &rect);
                 }
-                SDL_RenderFillRect(renderer, &rect);
+            }
+            if (currentState == GAME_OVER) {
+                drawText(renderer, "GUM! MENÜ ICIN TIKLA", 150, 280, {255, 0, 0, 255});
             }
         }
         SDL_RenderPresent(renderer);
